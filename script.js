97e3e23cc13a7b351fcf3aa8a669a8adb810070e
clemant <clemantcarlos11@gmail.com>
2025-06-13 15:24:54 -0400

    first commit

---
+++ script.js
@@ -0,0 +1,6 @@
+// Simple script to open project links in a new tab (if needed)
+document.querySelectorAll('.project-link').forEach(link => {
+  link.addEventListener('click', () => {
+    alert('Project details coming soon!');
+  });
+});
